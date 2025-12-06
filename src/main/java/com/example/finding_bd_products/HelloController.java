2ef1537d3000d119a22ac1696ceb93377a92a4f9
Fariha <fariha1032004@gmail.com>
2025-12-06 23:08:04 +0600

    Initial commit

---
+++ src/main/java/com/example/finding_bd_products/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.finding_bd_products;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
