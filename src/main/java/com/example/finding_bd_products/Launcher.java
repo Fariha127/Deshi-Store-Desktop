4f3c03c0765472f047e3b08792ed1fa0c763380a
Fariha <fariha1032004@gmail.com>
2025-12-06 22:16:02 +0600

    Initial commit

---
+++ src/main/java/com/example/finding_bd_products/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.finding_bd_products;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.finding_bd_products {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.finding_bd_products to javafx.fxml;
+    exports com.example.finding_bd_products;
+}
