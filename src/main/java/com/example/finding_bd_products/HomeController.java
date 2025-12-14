49f21ce53c8eeecc2fef02bcd073a0350c3f756d
Fariha <fariha1032004@gmail.com>
2025-12-15 00:58:30 +0600

    Modified Home.fxml and HomeController.java

--- src/main/java/com/example/finding_bd_products/HomeController.java
+++ src/main/java/com/example/finding_bd_products/HomeController.java
@@ -1,5 +1,30 @@
 package com.example.finding_bd_products;
 
-public class HomeController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
+
+public class HomeController {
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private Button homeBtn;
+
+    @FXML
+    private Button categoriesBtn;
+
+    @FXML
+    private Button newlyAddedBtn;
+
+    @FXML
+    private Button favouritesBtn;
+
+    @FXML
+    private GridPane recommendedGrid;
 }
